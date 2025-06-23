8dfd4b54cf47fb9eb3301bb78896cb4f5f3570f7
Prz <pedroaspires24@gmail.com>
2025-06-23 16:01:36 +0100

    First commit

---
+++ include/Date.hpp
@@ -0,0 +1,11 @@
+// date.hpp
+// This class is responsible for correctly defining dates for a certain task
+
+class Date{
+  public:
+    // Default constructor
+    Date();
+    // Constructor with parameters
+    
+
+};
---
+++ include/task.hpp
@@ -0,0 +1,12 @@
+#include <string>
+
+class Task{
+  public:
+    // Constructor with parameters
+    Task();
+  private:
+    // Name of the task
+    std::string taskName;
+    // Task due date
+    std::string dueDate;
+};
