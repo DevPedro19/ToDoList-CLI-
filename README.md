7d42d2a2bceef3d07b14d137bd0efe1b3bb5d168
Prz <pedroaspires24@gmail.com>
2025-08-13 18:09:37 +0100

    Updates - Fixed path problems and now we can build the file correctly

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ToDoList-CLI-
+## Download
+```bash
+git clone https://github.com/DevPedro19/ToDoList-CLI-.git
+cd ToDoList-CLI-.git
+```
+## How to run
+```bash
+cmake -B build
+cd build
+make
+./main
+
--- src/ToDoList.cpp
+++ src/ToDoList.cpp
@@ -19,7 +19,7 @@ ToDoList::ToDoList() = default;
 
 
 ToDoList::ToDoList(const string &filename) {
-    filePath = "../lists/" + filename + ".csv";
+    filePath = "lists/" + filename + ".csv";
     // Writes the header in the CSV
     WriteHeader();
     // Parse current file
